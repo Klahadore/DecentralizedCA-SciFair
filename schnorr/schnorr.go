9b5f907b5ce4ab6fd12d4fcbab75e9f0ab9e2fe6
Klahadore <stringycheese@protonmail.com>
2022-12-18 21:34:16 -0500

    Update schnorr.go

--- schnorr/schnorr.go
+++ schnorr/schnorr.go
@@ -1,50 +1,46 @@
 package schnorr
 
+// Schnorr signature schemes vary and are not necessarily compatible with each other
+// This implemntation follows the one outlined in FROST2020 [Komlo, Goldberg]
+
 import (
 	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/sha256"
 	"errors"
 	"math/big"
-	
 )
 
-// Generates random Hexadecimal nonce of type big.Int 
-func NonceGen() (*big.Int, error) {
-	byteSlice := make([]byte, 32)
+// Generates random Hexadecimal nonce of type big.Int
+func NonceGen() (*[]byte, error) {
+	byteSlice := make([]byte, 16)
 
 	_, err := rand.Read(byteSlice)
 	if err != nil {
 		return nil, err
 	}
 
-	nonce := new(big.Int)
-	nonce.SetBytes(byteSlice)
+	//nonce := new(big.Int)
+	//nonce.SetBytes(byteSlice)
 
-	return nonce, nil
+	return &byteSlice, nil
 
 }
 
-// Schnorr signature schemes vary and are not necessarily compatible with each other
-// this implemntation follows the one outlined in 
-func Sign(privateKey *big.Int, message [32]byte) ([64]byte, error) {
-	curve := elliptic.Curve.P256() 
-	// generate 256 bit random nonce 
+func Sign(privateKey *big.Int, message [32]byte) (*[64]byte, error) {
+	// instantiate secp
+	curve := elliptic.P256()
 
-	b := make([]byte, 32)
-	_, err := rand.Read(b)
+	k, err := NonceGen()
 	if err != nil {
-		fmt.Println(err)
-		return
+		return nil, err
 	}
 
-
-
-
+	// compute the commitment R = g^k ∈ G
+	gx, gy := curve.G
+	R, err := curve.ScalarBaseMult(gx * k)
+	if err != nil {
+		return nil, err
+	}
 
 }
-
-func Verify
-
-
-
