1f99076e21d487fb594f4ccbb15e78802ea65ae6
Klahadore <stringycheese@protonmail.com>
2022-12-18 21:10:19 -0500

    added NonceGen()

--- schnorr/schnorr.go
+++ schnorr/schnorr.go
@@ -6,20 +6,29 @@ import (
 	"crypto/sha256"
 	"errors"
 	"math/big"
-
+	
 )
 
+// Generates random Hexadecimal nonce of type big.Int 
+func NonceGen() (*big.Int, error) {
+	byteSlice := make([]byte, 32)
 
+	_, err := rand.Read(byteSlice)
+	if err != nil {
+		return nil, err
+	}
 
-var (
-	Curve := elliptic.Curve.P256() 
+	nonce := new(big.Int)
+	nonce.SetBytes(byteSlice)
 
-)
+	return nonce, nil
+
+}
 
 // Schnorr signature schemes vary and are not necessarily compatible with each other
 // this implemntation follows the one outlined in 
 func Sign(privateKey *big.Int, message [32]byte) ([64]byte, error) {
-
+	curve := elliptic.Curve.P256() 
 	// generate 256 bit random nonce 
 
 	b := make([]byte, 32)
